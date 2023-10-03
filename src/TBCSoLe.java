7a6df7a8f247e35d9ab2780045a42f2848ea9c18
lqt1893 <93394403+lqt1893@users.noreply.github.com>
2023-10-03 15:43:58 +0700

    added Bai 11 + 12

---
+++ src/TBCSoLe.java
@@ -0,0 +1,26 @@
+public class TBCSoLe {
+    private int n;
+    public float Tinh(){
+        int t=0, s=0;
+        while (n>0){
+            if (n%2==1)
+                t+=n%10;
+            s++;
+            n/=10;
+        }
+        return (float)t/s;
+    }
+    public float TinhDeQuy(int n){
+        if (n==0)
+            return 0;
+        if (n%10!=0)
+            return (n%10+TinhDeQuy(n/10))/2;
+        return TinhDeQuy(n/10);
+    }
+    public void setn(int n){
+        this.n=n;
+    }
+    public int getn(){
+        return n;
+    }
+}
